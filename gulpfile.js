43d96599a463b70eaab5ee3c2dc8351faec58cfd
keilermora <keilermora@gmail.com>
2019-06-23 15:53:54 -0400

    Elimina la dependencia a particlesjs

--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,6 @@ const scripts = {
   todos: [
     './node_modules/jquery/dist/jquery.min.js',
     './node_modules/bootstrap/dist/js/bootstrap.min.js',
-    './node_modules/particlesjs/dist/particles.min.js',
     './node_modules/swiper/dist/js/swiper.min.js',
     './src/scripts/index.js'
   ],
