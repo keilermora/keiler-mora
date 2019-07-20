ea9dd91d983f890db26255f17beec0d8bb971c83
keilermora <keilermora@gmail.com>
2019-07-20 12:21:57 -0400

    Refactor en el script y los nombres de las clases, sabiendo que la pagina ya no se renderiza con php

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,251 +1,158 @@
-/**
- * Determina si está sucediendo la animación del cambio de luces
- */
-var g_cambiandoHorario = false;
-/**
- * Determina si está sucediendo el cambio de idioma
- */
-var g_cambiandoIdioma = false;
-/**
- * Determina el idioma del sitio web
- */
-var g_idioma;
-
-/**
- * Función que muestra al usuario algún mensaje arrojado por el sitio web,
- * el idioma dependerá del seleccionado en el sitio web.
- * Los mensajes están escritos en los archivos json dentro de la carpeta locales/,
- * al menos que sea un error inesperado.
- * @function mostrarMensaje
- * @param {string} mensaje - Mensaje que mostrará el toast
- * @param {string} [clase] - Clase que tendrá el toast mostrado
- */
-function mostrarMensaje(mensaje, clase) {
-  if (!clase) {
-    clase = '';
-  }
-  else {
-    $('.alert').removeClass('alert-danger')
-     .removeClass('alert-success')
-     .addClass(clase);
-  }
-
-  //Cargar JSON del idioma y traducir el mensaje
-  $.getJSON('locales/' + g_idioma + '.json', function(data) {
-    if (data[mensaje] !== undefined) {
-      $('.alert').text(data[mensaje]);
-    }
-    else {
-      $('.alert').text(mensaje);
-    }
-
-    //Mostrar el Toast
-    TweenLite.to('.alert', 0.25, { opacity: 1, onComplete: function() {
-      setTimeout(function() {
-          //Ocultar el Toast
-          TweenLite.to('.alert', 0.25, { opacity: 0 });
-        }, 3000);
-    }});
-  });
-}
-
-$(document).ready(function() {
-
-  //Detectar idioma
-  g_idioma = $('#idiomas-dropdown').text() == 'Idioma' ? 'es' : 'en';
+(function() {
+  'use strict';
 
   /**
-   * Iniciar el slider
+   * Determina si está sucediendo la animación del cambio de luces
    */
-  var mySwiper = new Swiper ('.swiper-container', {
-    autoplay: {
-      delay: 5000,
-    },
-    speed: 800,
-    loop: true,
-    lazy: true,
-    effect: 'coverflow',
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    }
-  });
+  var g_changingDayTime = false;
+  /**
+   * Determina si está sucediendo el cambio de idioma
+   */
+  var g_changingLanguage = false;
+  /**
+   * Determina el idioma del sitio web
+   */
+  var g_language;
 
   /**
-   * Ajustar volumen de los audios
+   * Traduce el sitio web
+   * @param language 'es'|'en'
    */
-  $('audio').prop('volume', '0.1');
-
-  /**
-   * Enlaces del navbar
-   */
-  $('nav a').click(function(e) {
-    e.preventDefault();
-    var href = $(this).attr('href');
-
-    if(href) {
-      TweenLite.to(window, 1, {scrollTo: href });
-      $('#navbar-collapse').collapse('hide');
-    }
-  });
-
-  /**
-   * Botón para apagar/encender las luces
-   */
-  $('#btn-cambiar-horario').click(function() {
-    if(!g_cambiandoHorario) {
-
-      g_cambiandoHorario = true;
-
-      //Si es de día, cambiar a noche
-      if( $('body').hasClass('dia') ) {
-        $('.dia').removeClass('dia').addClass('noche');
-      }
-      //Si es de noche, cambiar a día
-      else {
-        $('.noche').removeClass('noche').addClass('dia');
-      }
-
-      g_cambiandoHorario = false;
-
-      //Cambiar el estado del botón de cambio de horario
-      $(this).toggleClass('active');
-    }
-  });
-
-  /**
-   * Botón para traducir el sitio web
-   */
-  $('.btn-traductor').click(function() {
-
-    //Cambiar idioma si no se está ejecutando un cambio del mismo
-    if(!g_cambiandoIdioma) {
-
-      //Capturar idioma
-      var idioma = $(this).attr('data-idioma');
-
-      //Cambiar idioma si es diferente al idioma actual
-      if(idioma != g_idioma) {
-
-        //Declarar el comienzo del cambio de idioma
-        g_cambiandoIdioma = true;
-        
-        TweenLite.to('.traducir', 0.25, { opacity: 0, onComplete: function() {
-
-          //Traducir el sitio web
-          $.getJSON('locales/' + idioma + '.json', function(data) {
-            $('.traducir').each(function() {
-              $(this).text(data[$(this).attr('data-key')]);
-            });
-
-            $('.traducir-placeholder').each(function() {
-              $(this).attr('placeholder', data[$(this).attr('data-key')]);
-            });
-
-            //Mostrar texto
-            TweenLite.to('.traducir', 0.5, { opacity: 1 });
-          });
-        }});
-
-        //Actualizar la variable global de idioma
-        g_idioma = idioma;
-
-        //Declarar el fin del cambio de idioma
-        g_cambiandoIdioma = false;
-      }
-    }
-  });
-
-  /**
-   * Hover a los logos en conocimientos
-   */
-  $('.conocimiento').hover(
-    function() {
-      $('#etiqueta').text( $(this).attr('data-etiqueta') );
-      TweenLite.to('#etiqueta', 0.5, { opacity: 1 });
-    }, function() {
-      TweenLite.to('#etiqueta', 0.5, { opacity: 0 })
-    }
-  );
-
-  /**
-   * Reproducir sonidos
-   */
-  $('#svg-triforce').click(function() {
-    $('#botw-secret').get(0).play();
-  });
-
-  /**
-   * Botón submit para el formulario de contacto
-   */
-  $('#form-contacto').submit(function(e) {
-    e.preventDefault();
-
-    var nombre  = $('#nombre').val(),
-        correo  = $('#correo').val(),
-        mensaje = $('#mensaje').val(),
-        cc      = $('#cc').prop('checked');
-
-    if( nombre == '' || correo == '' || mensaje == '' ) {
-      mostrarMensaje('error-campos-vacios', 'alert-danger');
-    }
-    else if( !/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(correo) ) {
-      mostrarMensaje('error-correo-invalido', 'alert-danger');
-    }
-    else {
-      //Almacenar el texto actual del botón, debido al cambio de idioma
-      var btnTexto = $('#btn-enviar').text();
-      //Cambiar el texto del botón por un spinner animado
-      $('#btn-enviar')
-        .html('<i class="fa fa-spin fa-refresh" aria-hidden="true"></i>')
-        .prop('disabled', true);
-
-      $.ajax({
-        url: 'functions/enviar_correo.php',
-        data: {
-          nombre: nombre,
-          correo: correo,
-          mensaje: mensaje,
-          cc: cc,
-          idioma: g_idioma
-        },
-        type: 'POST',
-        success: function(data) {
-
-          //Recuperar texto del botón
-          $('#btn-enviar').html(btnTexto).prop('disabled', false);
-
-          try {
-            var respuesta = JSON.parse(data);
-
-            switch(respuesta.estado) {
-              case 'correcto':
-
-                TweenLite.to(window, 1, {scrollTo: '#section-inicio' });
-                $('#form-contacto').trigger('reset');
-                mostrarMensaje(respuesta.descripcion, 'alert-success');
-
-              break;
-              case 'error':
-              default:
-                mostrarMensaje('error-inesperado', 'alert-danger');
-                console.log(respuesta.descripcion);
-              break;
-            }
-          }
-          catch (e) {
-            mostrarMensaje('error-inesperado', 'alert-danger');
-            console.log(data);
-          }
-        }
+  function translate(language) {
+    $.getJSON('locales/' + language + '.json', function(data) {
+      $('.traducir').each(function() {
+        $(this).text(data[$(this).attr('data-key')]);
       });
-    }
-  });
 
-  //Escribir mensaje en consola
-  console.log("%c¡Hola! Si deseas ver el código fuente con fines evaluativos o por curiosidad, visita https://github.com/keilermora/keiler-mora", "font-family: 'Arial'; font-size: 16px");
-});
+      $('.traducir-placeholder').each(function() {
+        $(this).attr('placeholder', data[$(this).attr('data-key')]);
+      });
+
+      //Mostrar texto
+      TweenLite.to('.traducir', 0.5, { opacity: 1 });
+    });
+  }
+
+  $(document).ready(function() {
+
+    g_language = 'es';
+
+    //Traducir el sitio web
+    translate('es');
+
+    /**
+     * Iniciar el slider
+     */
+    new Swiper ('.swiper-container', {
+      autoplay: {
+        delay: 5000,
+      },
+      speed: 800,
+      loop: true,
+      lazy: true,
+      effect: 'coverflow',
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      }
+    });
+
+    /**
+     * Ajustar volumen de los audios
+     */
+    $('audio').prop('volume', '0.1');
+
+    /**
+     * Enlaces del navbar
+     */
+    $('nav a').click(function(e) {
+      e.preventDefault();
+      var href = $(this).attr('href');
+
+      if(href) {
+        TweenLite.to(window, 1, { scrollTo: href });
+        $('#navbar-collapse').collapse('hide');
+      }
+    });
+
+    /**
+     * Botón para apagar/encender las luces
+     */
+    $('#btn-cambiar-horario').click(function() {
+      console.log('click?');
+      if(!g_changingDayTime) {
+
+        g_changingDayTime = true;
+
+        //Si es de día, cambiar a noche
+        if( $('body').hasClass('day') ) {
+          $('.day').removeClass('day').addClass('night');
+        }
+        //Si es de noche, cambiar a día
+        else {
+          $('.night').removeClass('night').addClass('day');
+        }
+
+        g_changingDayTime = false;
+
+        //Cambiar el estado del botón de cambio de horario
+        $(this).toggleClass('active');
+      }
+    });
+
+    /**
+     * Botón para traducir el sitio web
+     */
+    $('.btn-traductor').click(function() {
+
+      //Cambiar idioma si no se está ejecutando un cambio del mismo
+      if(!g_changingLanguage) {
+
+        //Capturar idioma
+        var language = $(this).attr('data-idioma');
+
+        //Cambiar idioma si es diferente al idioma actual
+        if(language !== g_language) {
+
+          //Declarar el comienzo del cambio de idioma
+          g_changingLanguage = true;
+
+          TweenLite.to('.traducir', 0.25, { opacity: 0, onComplete: function() {
+            translate(language)
+          }});
+
+          //Actualizar la variable global de idioma
+          g_language = language;
+
+          //Declarar el fin del cambio de idioma
+          g_changingLanguage = false;
+        }
+      }
+    });
+
+    /**
+     * Hover a los logos en conocimientos
+     */
+    $('.conocimiento').hover(function() {
+        $('#etiqueta').text( $(this).attr('data-etiqueta') );
+        TweenLite.to('#etiqueta', 0.5, { opacity: 1 });
+      }, function() {
+        TweenLite.to('#etiqueta', 0.5, { opacity: 0 })
+      }
+    );
+
+    /**
+     * Reproducir sonidos
+     */
+    $('#svg-triforce').click(function() {
+      $('#botw-secret').get(0).play();
+    });
+
+    //Escribir mensaje en consola
+    console.log("%c¡Hola! Si deseas ver el código fuente con fines evaluativos o por curiosidad, visita https://github.com/keilermora/keiler-mora", "font-family: 'Arial'; font-size: 16px");
+  });
+})();
